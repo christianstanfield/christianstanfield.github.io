ecddb80cf8ef153c313f68cebae8f230940aad7a
Christian Stanfield <Ein@MacBook-Pro.local>
2014-12-29 22:05:45 -0600

    header working except for font matching

--- new-design/fonts.js
+++ new-design/fonts.js
@@ -1,4 +1,13 @@
-var randomizeFont = function (word) {
+// array contains method:
+Array.prototype.contains = function ( needle ) {
+  for (var i in this) {
+    if (this[i] == needle) return true;
+  }
+  return false;
+};
+
+// returns a random font
+var randomFont = function () {
 
   var fonts = ['Raleway',
                'Lora',
@@ -6,49 +15,144 @@ var randomizeFont = function (word) {
                'Times New Roman',
                'Arial'];
 
+  return fonts[Math.floor(Math.random() * fonts.length)];
+};
+// changes each letter of a word to a different font
+var randomizeFont = function (word) {
+
   var letters = $(word).text().split('');
+  var newWord = '';
 
-  setTimeout(function () {
-    $(word).text('');
-
-    $.each(letters, function (i, letter) {
-      var randomFont = fonts[Math.floor(Math.random() * fonts.length)];
-      $(word).append('<span style="font-family: ' + randomFont + '">' + letter + '</span>');
-    });
-  }, 500);
+  $.each(letters, function (i, letter) {
+    newWord += '<span style="font-family: ' + randomFont() + '">' + letter + '</span>';
+  });
+  $(word).html(newWord);
 };
 
-var randomizeColor = function (object) {
+// returns any number of random colors based on the parameter
+var randomColors = function (numOfColors) {
 
-  var colors = ['rgba(255, 145, 140, 0.93)',
-                'rgba(255, 102, 211, 1)',
-                'rgba(109, 104, 255, 0.93)',
-                'rgba(155, 250, 155, 1)',
-                'rgb(255, 249, 247)'];
+  var colors = ['rgb(245, 245, 245)',
+                'rgb(53, 103, 135)',
+                'rgb(33, 33, 33)',
+                'rgb(23, 45, 59)'];
+  var randomColors = [];
 
-  var randomColor = colors[Math.floor(Math.random() * colors.length)];
-  $(object).css('background-color', randomColor);
-  $(object).css('border-color', randomColor);
+  if (numOfColors >= colors.length || numOfColors === 'all') {
+    return colors;
+  } else {
+    while (randomColors.length < numOfColors) {
+      var randomColor = colors[Math.floor(Math.random() * colors.length)];
+      if (randomColors.contains(randomColor)) {
+      } else {
+        randomColors.push(randomColor);
+      }
+    }
+    return randomColors;
+  }
+};
+// sets colors for an object
+var setColor = function (object, color1, color2) {
+
+  var $h1 = $(object).children();
+  $h1.css('background-color', color1);
+  $h1.css('border-color', color1);
+  $h1.css('color', color2);
+};
+// changes colors of an object's or objects' children
+var setColors = function (objects, colors) {
+
+  $.each(objects, function (i, object) {
+    var j = i-1;
+    if (j < 0) {
+      j = objects.length+1;
+    }
+    setColor(object, colors[i], colors[j]);
+  });
+};
+// returns background colors of an object's siblings and its own
+var siblingColors = function (object) {
+
+  var colors = [];
+
+  var $siblings = $(object).siblings().children();
+  $.each($siblings, function (i, sibling) {
+    colors.push( $(sibling).css('background-color') );
+  });
+
+  var currentColor = $(object).children().css('background-color');
+  colors.push(currentColor);
+  return colors;
+};
+// returns unused colors from parameter
+var unusedColors = function (usedColors) {
+
+  var colors = [];
+  $.each(randomColors('all'), function (i, color) {
+    if (usedColors.contains(color)) {
+    } else {
+      colors.push(color);
+    }
+  });
+
+  while (colors.length < 2) {
+    var randomColor = randomColors(1);
+    if (colors.contains(randomColor)) {
+    } else {
+      colors.push(randomColor);
+    }
+  }
+  return colors;
+};
+// return a different color from the parameter
+var differentColor = function (color) {
+  var newColor = '';
+  while (newColor === '') {
+    var randomColor = randomColors(1);
+    if (color != randomColor) {
+      newColor = randomColor;
+    }
+  }
+  return newColor;
 };
 
-var rotateImage = function (image) {
-  
-}
+var loadHeader = function () {
 
-$(function() {
+  $('header img').css('border-color', randomColors(1));
+
+  var $title = $('#title div');
+  var colors = randomColors($title.length);
+  setColors($title, colors);
 
   $('h1').each(function () {
     randomizeFont(this);
-    randomizeColor(this);
   });
-  randomizeColor('header img');
+};
 
-  $('h1').on('mouseenter', function () {
-    randomizeFont(this);
-    randomizeColor(this);
+$(function() {
+
+  // on page load:
+  loadHeader();
+
+  // on mouseover:
+  $('#title div').on('mouseenter', function () {
+    var colors = unusedColors( siblingColors(this) );
+    setColors([this], colors);
+  });
+
+  $('#title div').on('mouseleave', function () {
+    $(this).children().each(function () {
+      randomizeFont(this);
+    });
   });
 
   $('header img').on('mouseenter', function () {
+    var currentColor = $(this).css('border-color');
+    $(this).css('border-color', differentColor(currentColor));
+    $(this).toggleClass('rotated');
+  });
 
+  $('header img').on('mouseleave', function () {
+    $(this).toggleClass('rotated');
   });
 });
