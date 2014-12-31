67f344ea5ae52b1d597aaece39af81ea08222b39
Christian Stanfield <Ein@MacBook-Pro.local>
2014-12-31 14:24:20 -0600

    nebula scrolling working

---
+++ new-design/parallax.js
@@ -0,0 +1,23 @@
+$(function () {
+
+  // Init Skrollr
+  var s = skrollr.init({
+    forceHeight: false
+  });
+
+
+  // Get window size
+  // var winH = $(window).height();
+  //
+  // // Keep minimum height 550
+  // if(winH <= 550) {
+  //   winH = 550;
+  // }
+
+  // Resize our slides
+  // $('.slide').height(550);
+
+  // Refresh Skrollr after resizing our sections
+
+
+});
